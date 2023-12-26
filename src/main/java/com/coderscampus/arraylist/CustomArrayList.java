7c13a874ac107acd61fc49cf9c63cdcb58decddd
Kevin Gallaccio <gallacciokevin@gmail.com>
2023-12-26 23:04:25 +0100

    initial commit for Assignment 5

--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -5,19 +5,16 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	@Override
 	public boolean add(T item) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public int getSize() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public T get(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
