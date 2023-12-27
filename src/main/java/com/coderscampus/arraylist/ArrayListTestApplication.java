c1e99d677c91e39b2ef3c6bea2f360ce180ce82a
Kevin Gallaccio <gallacciokevin@gmail.com>
2023-12-27 21:21:37 +0100

    Final Commit of Assignment 5

---
+++ src/main/java/com/coderscampus/arraylist/ArrayListTestApplication.java
@@ -0,0 +1,32 @@
+package com.coderscampus.arraylist;
+
+public class ArrayListTestApplication {
+
+	public static void main(String[] args) {
+
+		CustomList<Integer> myCustomList = new CustomArrayList<>();
+
+		
+		// Chose the max number of items to be added to the list below :
+		
+		Integer myCustomListSize = 60;
+		
+		for (Integer i = 1; i <= myCustomListSize; i++) {
+			myCustomList.add(i);
+		}
+		
+		// test the size :
+
+		System.out.println("CustomArrayList size is -> " + myCustomList.getSize());
+
+		System.out.println("- - - - -");
+		
+		// print all :
+
+		for (Integer i = 0; i < myCustomList.getSize(); i++) {
+			System.out.println("index " + i + " -> " + myCustomList.get(i));
+		}
+
+	}
+
+}
--- src/main/java/com/coderscampus/arraylist/CustomArrayList.java
+++ src/main/java/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,21 +1,44 @@
 package com.coderscampus.arraylist;
 
+import java.util.Arrays;
+
 public class CustomArrayList<T> implements CustomList<T> {
+
 	Object[] items = new Object[10];
+	Integer arraySize = 0;
 
 	@Override
 	public boolean add(T item) {
-		return false;
+		if (arraySize < items.length || arraySize % 10 == 0) {
+			if (arraySize % 10 == 0) {
+			Object[] itemsUpdated = Arrays.copyOf(items, getSize() + 10);
+			items = itemsUpdated;
+		} 
+		items[arraySize] = item;
+		arraySize++;
+		return true;
+		} else {
+			return false;
+		}
 	}
 
+	
 	@Override
 	public int getSize() {
-		return 0;
+		return arraySize;
 	}
 
+	
 	@Override
 	public T get(int index) {
+		if (index >= 0 && index < arraySize) {
+			@SuppressWarnings("unchecked")
+			T item = (T) items[index];
+			return item;
+		} else {
+			System.out.println("index is out of bounds");
+		}
 		return null;
 	}
-	
+
 }
